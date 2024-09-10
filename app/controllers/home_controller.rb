1f0b679a592baba902fa83b0753f6f4ac184b3cd
mametaro99 <bearsmichimato99@gmail.com>
2024-09-10 05:49:28 +0000

    学生証のアップロード機能を作成しました。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: [:upload_student_card]
+
   def index
+    # Home#indexのアクション内容
+  end
+
+  def upload_student_card
+    current_user.student_card_front.attach(params[:user][:student_card_front])
+    current_user.student_card_back.attach(params[:user][:student_card_back])
+    redirect_to root_path, notice: 'Student card uploaded successfully.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'upload_student_card', to: 'home#upload_student_card'
 end
